8f9eb51e828aeddc098ee260e6e81e81738ddccd
rodrigocenteno <rodrigo.alejandroc@hotmail.com>
2020-06-25 11:29:35 -0600

    tercer commit

--- assets/js/js.js
+++ assets/js/js.js
@@ -46,18 +46,6 @@ $(document).ready(function(){
   
 });
 
-window.addEventListener("load", function(){
-  var template = document.getElementById("article-template");
-  var articles = document.getElementById("articles");
 
-  for(var i = 1; i <= 5; i++){
-      var clonado = template.cloneNode(true);
-      clonado.removeAttribute("id");
 
-      var h2 = clonado.getElementsByTagName("h2")[0];
-      h2.innerHTML = h2.textContent + ' ' + i;
 
-      articles.appendChild(clonado);
-  }
-
-});
